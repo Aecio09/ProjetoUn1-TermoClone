487829fceab54f4ff7ce67f6b9b62965a9c47b93
Aecio <aeciojunior68@gmail.com>
2025-01-25 13:36:27 -0300

    bontoes bonitos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,13 @@
   import "../style/Paginas.css"
 </script>
 
+<body>
 
+</body>
 <nav>LETRIX</nav>
 
 <div class="container">
 <a class="play" href="/Play">PLAY</a>
-</div>
 
 <a class="play2" href="/About">ABOUT</a>
-
-<body>
-    
-</body>
+</div>
